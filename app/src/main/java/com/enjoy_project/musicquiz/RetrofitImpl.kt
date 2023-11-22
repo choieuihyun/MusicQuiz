8ba4a32c41b3d081735d25269e23b8f8ca3197a7
choeuihyeon <amammmaa1@naver.com>
2023-11-22 23:26:08 +0900

    feature : 정보 입력 화면에서 데이터 전송 형식 변경으로 인한 코드 추가

--- app/src/main/java/com/enjoy_project/musicquiz/RetrofitImpl.kt
+++ app/src/main/java/com/enjoy_project/musicquiz/RetrofitImpl.kt
@@ -88,6 +88,31 @@ class RetrofitImpl {
 
     }
 
+    suspend fun addUserTeam(id: Int?, teamName: String, teamNumber: Int?) {
+
+        try {
+
+            RetrofitClient.userService.addUserTeam(id, teamName, teamNumber)
+                .enqueue(object : Callback<Void?> {
+                    override fun onResponse(call: Call<Void?>, response: Response<Void?>) {
+                        if (response.isSuccessful) {
+                            Log.d("retrofitImplPost", "success")
+                        } else
+                            Log.d("retrofitImplPost", response.message())
+                    }
+
+                    override fun onFailure(call: Call<Void?>, t: Throwable) {
+                        Log.e("retrofitImplPost", "연결 실패")
+                    }
+
+                })
+
+        }catch (e: NullPointerException) {
+            Log.d("NPE", e.message!!)
+        }
+    }
+
+
     fun getSong(id: Int, callback: (Songs?) -> Unit) {
 
         RetrofitClient.userService.getSong(id).enqueue(object : Callback<Songs> {
